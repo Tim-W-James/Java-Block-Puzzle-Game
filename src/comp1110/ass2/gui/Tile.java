f7c50318e95d5262fbc71dfeeb2618b28076e481
Rebecca <u6982060@anu.edu.au>
2019-08-19 09:31:23 +1000

    Tile Class added. Some methods added; need to make the respective classes, in order for it to work

---
+++ src/comp1110/ass2/gui/Tile.java
@@ -0,0 +1,32 @@
+package comp1110.ass2.gui;
+
+public class Tile {
+
+    private TileType t;
+    private Position p;
+    private Orientation o;
+
+    public Tile (String placement) {
+        this.t  = ;
+        this.p = ;
+        this. o = ;
+
+    }
+
+    public TileType getTileType() {
+        return t;
+    }
+
+    public Position getPosition() {
+        return p;
+    }
+
+    public Orientation getOrientation() {
+        return o;
+    }
+
+    //getOrientation
+    //getPosition (x,y coordinates - where X is the leftmost square, Y is the row where the top square of the piece is in)
+    //getTileType (a,b,c,d.. etc.)
+
+}
