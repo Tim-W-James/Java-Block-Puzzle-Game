bc923617cf577653dbec51ded35c8b158013f471
timja <u6947396@anu.edu.au>
2019-08-19 14:38:45 +1000

    Added useful methods to Tile, GameBoardArray, Position. Minor fixes. Lots of toString() improvements to help with debugging

--- src/comp1110/ass2/GameBoardArray.java
+++ src/comp1110/ass2/GameBoardArray.java
@@ -1,31 +1,49 @@
 package comp1110.ass2;
 
+import java.util.EmptyStackException;
+
+import static comp1110.ass2.State.*;
+
 public class GameBoardArray {
 
-    private State[][] board;
+    // initially all states are empty, except (0,4) and (8,4) which are unused
+    private State[][] gameBoard = {
+                {EMPTY, EMPTY, EMPTY, EMPTY, UNUSED},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, UNUSED}
+        };
 
-    public GameBoardArray() {
-        // TODO
-        // Set some stuff
-    }
+     // Get and Set methods for the class
 
+    public State[][] getBoardState() { return gameBoard; }
 
-
-    // Get and Set methods for the class
-
-    public State[][] getBoardState() {
-        // TODO
-        return board;
-    }
+    // find the state at a given position
+    public State getStateAt(Position pos) { return gameBoard[pos.getX()][pos.getY()]; }
+    public State getStateAt(int x, int y) { return gameBoard[x][y]; }
 
     public void updateBoardPosition(Tile t) {
-        // TODO
-        // Updates the board with a tile
+        // Check position is valid
+        if (checkValidPosition(t))
+            throw new IllegalArgumentException("Invalid Tile Input");
+
+        // TODO updateBoardPosition should update the board with a tile
     }
 
     public boolean checkValidPosition(Tile t) {
-        // TODO
-        // Checks if a tile can go in a given board position
+        // TODO checkValidPosition should check if a tile can go in a given board position
         return false;
+    }
+
+    @Override
+    public String toString() {
+        // TODO toString should pretty print the array for easy debugging
+        return super.toString();
     }
 }
--- src/comp1110/ass2/Tile.java
+++ src/comp1110/ass2/Tile.java
@@ -2,13 +2,17 @@ package comp1110.ass2;
 
 import static comp1110.ass2.Direction.*;
 
-
 public class Tile {
 
     private Shape shape;
     private Position pos;
     private Direction dir;
     private String placement;
+    static private String invalidTileMsg = "Invalid Placement Input, \n\tPlease ensure:\n" +
+            "\t- the first character is in the range a .. j (shape)\n" +
+            "\t- the second character is in the range 0 .. 8 (column)\n" +
+            "\t- the third character is in the range 0 .. 4 (row)\n" +
+            "\t- the fourth character is in the range 0 .. 3 (orientation)";
 
     public Tile (String piecePlacement) {
         if (FocusGame.isPiecePlacementWellFormed(piecePlacement)) {
@@ -18,10 +22,10 @@ public class Tile {
             this.placement = piecePlacement;
         }
         else
-            throw new IllegalArgumentException("Invalid Placement Input");
+            throw new IllegalArgumentException(invalidTileMsg);
     }
 
-    public Shape getTileType() {
+    public Shape getShape() {
         return shape;
     }
 
@@ -35,20 +39,29 @@ public class Tile {
 
     public String getPlacement() { return placement; }
 
-    public static Shape placementToShape (String placement) {
+    public static Shape placementToShape (String piecePlacement) {
+        if (!FocusGame.isPiecePlacementWellFormed(piecePlacement)) // check input is valid
+            throw new IllegalArgumentException(invalidTileMsg);
+
         // shape indexed at [0]
-        return Shape.valueOf(Character.toString((placement.charAt(0) - 32)).toUpperCase());
+        return Shape.valueOf(Character.toString((piecePlacement.charAt(0) - 32)).toUpperCase());
     }
 
-    public static Position placementToPosition (String placement) {
+    public static Position placementToPosition (String piecePlacement) {
+        if (!FocusGame.isPiecePlacementWellFormed(piecePlacement)) // check input is valid
+            throw new IllegalArgumentException(invalidTileMsg);
+
         // position indexed at x: [1] and y: [2]
         return new Position(
-            Character.getNumericValue(placement.charAt(1)),
-            Character.getNumericValue(placement.charAt(2)));
+            Character.getNumericValue(piecePlacement.charAt(1)),
+            Character.getNumericValue(piecePlacement.charAt(2)));
     }
 
-    public static Direction placementToDirection (String placement) {
-        switch (placement.charAt(3)) { // direction indexed at [3]
+    public static Direction placementToDirection (String piecePlacement) {
+        if (!FocusGame.isPiecePlacementWellFormed(piecePlacement)) // check input is valid
+            throw new IllegalArgumentException(invalidTileMsg);
+
+        switch (piecePlacement.charAt(3)) { // direction indexed at [3]
             case '0':
                 return NORTH;
 
@@ -61,5 +74,10 @@ public class Tile {
             default:
                 return WEST;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Shape: "+shape+", X: "+pos.getX()+", Y: "+pos.getY()+", Dir: "+dir;
     }
 }
