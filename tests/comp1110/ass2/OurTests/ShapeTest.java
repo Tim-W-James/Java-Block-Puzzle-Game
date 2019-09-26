46293738983fedd8fc90c03be2e5a0e15cf42a65
goshdarnitnick <ShadowHawk07@users.noreply.github.com>
2019-09-26 10:36:54 +1000

    Added more tests to ShapeTest

--- tests/comp1110/ass2/OurTests/ShapeTest.java
+++ tests/comp1110/ass2/OurTests/ShapeTest.java
@@ -2,11 +2,16 @@ package comp1110.ass2.OurTests;
 
 import comp1110.ass2.Direction;
 import comp1110.ass2.Shape;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import static org.junit.Assert.*;
 
 public class ShapeTest {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(500);
 
     @Test
     public void getMaxReach() {
@@ -31,5 +36,41 @@ public class ShapeTest {
         assertEquals("Max vert reach for I facing west should be 2",
                 2,
                 Shape.I.getMaxReach(Direction.WEST, true));
+    }
+
+    @Test
+    public void returnCorrectLetters() {
+        assertEquals("Shape A returns 'a'", 'a',
+                Shape.A.toChar());
+        assertEquals("Shape B returns 'b'", 'b',
+                Shape.B.toChar());
+        assertEquals("Shape C returns 'c'", 'c',
+                Shape.C.toChar());
+        assertEquals("Shape D returns 'd'", 'd',
+                Shape.D.toChar());
+        assertEquals("Shape E returns 'e'", 'e',
+                Shape.E.toChar());
+        assertEquals("Shape F returns 'f'", 'f',
+                Shape.F.toChar());
+        assertEquals("Shape G returns 'g'", 'g',
+                Shape.G.toChar());
+        assertEquals("Shape H returns 'h'", 'h',
+                Shape.H.toChar());
+        assertEquals("Shape I returns 'i'", 'i',
+                Shape.I.toChar());
+        assertEquals("Shape J returns 'j'", 'j',
+                Shape.J.toChar());
+
+    }
+
+    @Test
+    public void createShapeFromChar() {
+        assertEquals("Shape A created from 'a'", Shape.A,
+                Shape.charToShape('a'));
+        assertEquals("Shape E created from 'e'", Shape.E,
+                Shape.charToShape('e'));
+        assertEquals("Shape J created from 'j'", Shape.J,
+                Shape.charToShape('j'));
+
     }
 }
